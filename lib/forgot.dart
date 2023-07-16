1e747fd61c412f1d98e010488793509d072f771c
penguin <penguin.qlc@gmail.com>
2023-07-16 11:40:29 +0300

    reset

--- lib/forgot.dart
+++ lib/forgot.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d14a1abe30b1d26b20023760e5529adb0bb49b0b9ad8d2649016a0f63e1bac1
-size 2126
+oid sha256:40646dd5b3dcba07d96c4315f385380ab2cf262fa27643ad0419fc34772fa2f2
+size 3236
