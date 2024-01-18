fc3ca699c794a794408437b8c1e696cf107b8160
penguin <penguin.qlc@gmail.com>
2024-01-18 13:38:17 +0300

    test

--- lib/card.dart
+++ lib/card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:608c060cdab59bbd689524c807cd789bd95876714f1166ccd23d927a84f25fa6
-size 886
+oid sha256:f5c2967dfe0377f117725f06ff1251705b379ee78d87f59b1433dbc86e7c00d4
+size 890
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6820aa7794e4105c83816ee20fdf6102c8df4f13f25337fb53136b0734cb78c
-size 3703
+oid sha256:1d2feee49b5558311c533610413e8fefb0f6d17225d6eca6d075ff35a1bd809a
+size 3670
